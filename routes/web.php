38578c44772d394cc36166a65bb44efb9d29d4a9
curicdomagoj <curicdomagoj993@gmail.com>
2017-05-10 21:44:18 +0200

    ruta za ispis studenata

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get("/student", "\App\Http\Controllers\StudController@pozdrav");
+
+Route::get("/studenti", "\App\Http\Controllers\StudController@lista");
